bfc2afbe58742c70c61ffbba888945f8cd98e3ef
Cory Bateman <splintercode.cb@gmail.com>
2014-02-04 12:41:09 -0600

    Added temp push array

--- Content/Scripts/scripts.js
+++ Content/Scripts/scripts.js
@@ -1,6 +1,8 @@
 ﻿function TimerCtrl($scope) {
 
-    $scope.options = [{ value: 25, label: 25 }, { value: 20, label: 20 }, { value: 15, label: 15 }, { value: 10, label: 10 }];
+    $scope.options = [{ value: 1, label: 1 },{ value: 25, label: 25 }, { value: 20, label: 20 }, { value: 15, label: 15 }, { value: 10, label: 10 }];
+    $scope.results = [{ start: '12:24 PM', stop: '12:44 PM' }];
+
     $scope.selectedOption = $scope.options[1];
     $scope.currentTime = $scope.selectedOption.value + ":" + "00";
     $scope.playState = "Play";
@@ -37,21 +39,28 @@
     function intervalTimer() {
         if (timerDate.getMinutes() === 0 && timerDate.getSeconds() === 0) {
             $scope.stopTimer();
-            alertNotification();          
+            alertNotification();
+            pushResult();
         }
         else {
             timerDate.setSeconds(timerDate.getSeconds() - 1);
             $scope.currentTime = timerDate.getMinutes() + ":" + timerDate.getSeconds();
-            document.title = $scope.currentTime;
-            $scope.$apply()
+            document.title = $scope.currentTime;            
         }
+        $scope.$apply();
     };
+
+    function pushResult() {
+        $scope.results.push({ start: '09:10 AM', stop: '09:30 AM' });
+    }
 
     function resetTimer() {
         clearInterval(timerInterval);
         $scope.currentTime = $scope.selectedOption.value + ":" + "00";
-        timerDate.setMinutes($scope.selectedOption.value);
-        timerDate.setSeconds(0);
+        //timerDate.setMinutes($scope.selectedOption.value);
+        //timerDate.setSeconds(0);
+        timerDate.setMinutes(0);
+        timerDate.setSeconds(1);
     };
 
     function alertNotification() {
@@ -67,7 +76,7 @@
 
     function alertDesktopNotification() {
         if (window.webkitNotifications.checkPermission() == 0) {
-            notification_test = window.webkitNotifications.createNotification(
+            var notification_test = window.webkitNotifications.createNotification(
             'Content/pom.png', 'Agility!', 'Time to take a break!');
             notification_test.ondisplay = function () { };
             notification_test.onclose = function () { };
