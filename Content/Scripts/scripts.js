063cedc6a1b8a670f116c821e618a8ff389f1739
Cory Bateman <splintercode.cb@gmail.com>
2014-02-05 00:11:37 -0600

    New design

--- Content/Scripts/scripts.js
+++ Content/Scripts/scripts.js
@@ -1,46 +1,44 @@
 ﻿function TimerCtrl($scope) {
 
     $scope.options = [{ value: 1, label: 1 },{ value: 25, label: 25 }, { value: 20, label: 20 }, { value: 15, label: 15 }, { value: 10, label: 10 }];
-    $scope.results = [{ start: '12:24 PM', stop: '12:44 PM' }];
+    $scope.tasks = [];
 
     $scope.selectedOption = $scope.options[1];
     $scope.currentTime = $scope.selectedOption.value + ":" + "00";
     $scope.playState = "Play";
+    $scope.isPlaying = false;
 
-    var isPauseState = false;
+    var startTime = new Date().toLocaleTimeString();
+    var endTime = new Date().toLocaleTimeString();
     var timerInterval = 0;
     var desktopNotificationCalled = false;
-
     var timerDate = new Date();
         timerDate.setMinutes($scope.selectedOption.value);
         timerDate.setSeconds(0);
 
     $scope.startTimer = function () {
-        if (isPauseState === false) {
-            isPauseState = true;
-            $scope.playState = "Pause";
-            timerInterval = setInterval(intervalTimer, 1000);
-        }
-        else {
-            isPauseState = false;
-            $scope.playState = "Play";
-            clearInterval(timerInterval);
-        }
+        resetTimer();
+        timerInterval = setInterval(intervalTimer, 1000);
+        startTime = new Date().toLocaleTimeString();
+        $scope.isPlaying = true;
     };
 
     $scope.stopTimer = function () {
-        isPauseState = false;
-        $scope.playState = "Play";
         resetTimer();
+        $scope.isPlaying = "";
     };
 
+    $scope.clearList = function () {
+        $scope.tasks = [];
+    };
 
     /* --- Helper Functons --- */
     function intervalTimer() {
         if (timerDate.getMinutes() === 0 && timerDate.getSeconds() === 0) {
             $scope.stopTimer();
+            endTime = new Date().toLocaleTimeString();
             alertNotification();
-            pushResult();
+            pushTask();
         }
         else {
             timerDate.setSeconds(timerDate.getSeconds() - 1);
@@ -50,17 +48,16 @@
         $scope.$apply();
     };
 
-    function pushResult() {
-        $scope.results.push({ start: '09:10 AM', stop: '09:30 AM' });
-    }
+    function pushTask() {
+        $scope.tasks.push({ start: startTime, stop: endTime });
+    };
 
     function resetTimer() {
         clearInterval(timerInterval);
+        desktopNotificationCalled = false;
         $scope.currentTime = $scope.selectedOption.value + ":" + "00";
-        //timerDate.setMinutes($scope.selectedOption.value);
-        //timerDate.setSeconds(0);
-        timerDate.setMinutes(0);
-        timerDate.setSeconds(1);
+        timerDate.setMinutes($scope.selectedOption.value);
+        timerDate.setSeconds(0); // Switch
     };
 
     function alertNotification() {
@@ -72,21 +69,34 @@
         if (desktopNotificationCalled === false) {
             alertDesktopNotification();
         }
-    }
+    };
 
     function alertDesktopNotification() {
-        if (window.webkitNotifications.checkPermission() == 0) {
-            var notification_test = window.webkitNotifications.createNotification(
-            'Content/pom.png', 'Agility!', 'Time to take a break!');
-            notification_test.ondisplay = function () { };
-            notification_test.onclose = function () { };
-            notification_test.show();
-            desktopNotificationCalled = true;
+        if (window.webkitNotifications) {
+            if (window.webkitNotifications.checkPermission() == 0) {
+                var notification_test = window.webkitNotifications.createNotification(
+                'Content/pom.png', 'Agility!', 'Time to take a break!');
+                notification_test.ondisplay = function () { };
+                notification_test.onclose = function () { };
+                notification_test.show();
+                desktopNotificationCalled = true;
+            }
         }
 
         //Close Desktop Notification after 1 min
         setTimeout(function () {
             notification_test.cancel();
         }, 60000);
-    }
+    };
+
+    (function RequestDesktopNotification() {
+        // If Webkit and supports Desktop Notifications Request Permission
+        if (window.webkitNotifications){
+            if (window.webkitNotifications.checkPermission() === 0) { // 0 is PERMISSION_ALLOWED
+                // function defined 
+            } else {
+                window.webkitNotifications.requestPermission();
+            }
+        }
+    }());
 }
