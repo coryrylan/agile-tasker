51c29eb90fef28404381192af65772bdde23f852
Cory Bateman <splintercode.cb@gmail.com>
2014-02-07 01:19:49 -0600

    New Features and settings/sound

--- Content/Scripts/scripts.js
+++ Content/Scripts/scripts.js
@@ -1,20 +1,26 @@
 ﻿function TimerCtrl($scope) {
 
-    $scope.options = [{ value: 15, label: 15 }, { value: 20, label: 20 }, { value: 25, label: 25 }, { value: 30, label: 30 }, { value: 1, label: 1 }, ];
+    $scope.options = [{ value: 15, label: 15 }, { value: 20, label: 20 }, { value: 25, label: 25 }, { value: 30, label: 30 }];
     $scope.tasks = [];
     $scope.selectedOption = $scope.options[1];
     $scope.currentTime = $scope.selectedOption.value + ":" + "00";
+    $scope.localTime = "";
     $scope.taskText = "";
     $scope.isPlaying = false;
+    $scope.settingVisible = true;
+    $scope.sound = true;
 
+    var snd1 = new Audio("Content/chime.mp3"); // buffers automatically when created
+    var snd2 = new Audio("Content/chime.wav"); // buffers automatically when created
     var startTime = new Date().toLocaleTimeString();
     var endTime = new Date().toLocaleTimeString();
     var timerInterval = 0;
     var timerDate = new Date();
-        timerDate.setMinutes($scope.selectedOption.value);
-        timerDate.setSeconds(0);
+    timerDate.setMinutes($scope.selectedOption.value);
+    timerDate.setSeconds(0);
 
-    $scope.startTimer = function() {
+    /* --- Click Events ---*/
+    $scope.startTimer = function () {
         resetTimer();
         timerInterval = setInterval(intervalTimer, 1000);
         startTime = new Date().toLocaleTimeString();
@@ -31,6 +37,23 @@
     };
 
     /* --- Helper Functons --- */
+    setInterval(updateLocalTime, 1000);
+    function updateLocalTime() {
+        $scope.localTime = toLocalTime(new Date());
+        $scope.$apply();
+    }
+
+    function toLocalTime(date) {
+        var h = date.getHours();
+        var m = date.getMinutes();
+        var x = h >= 12 ? 'pm' : 'am';
+        h = h % 12;
+        h = h ? h : 12;
+        m = m < 10 ? '0' + m : m;
+        var mytime = h + ':' + m + ' ' + x;
+        return mytime;
+    }
+
     function intervalTimer() {
         if (timerDate.getMinutes() === 0 && timerDate.getSeconds() === 0) {
             $scope.stopTimer();
@@ -41,9 +64,16 @@
         else {
             timerDate.setSeconds(timerDate.getSeconds() - 1);
             $scope.currentTime = getCurrentTime(timerDate);
-            document.title = $scope.currentTime;            
+            document.title = $scope.currentTime;
         }
         $scope.$apply();
+    };
+
+    function resetTimer() {
+        clearInterval(timerInterval);
+        $scope.currentTime = $scope.selectedOption.value + ":" + "00";
+        timerDate.setMinutes($scope.selectedOption.value);
+        timerDate.setSeconds(0); // Test Switch 
     };
 
     function getCurrentTime(currentTime) {
@@ -60,22 +90,20 @@
     }
 
     function pushTask() {
-        $scope.tasks.push({ start: startTime, stop: endTime, text: $scope.taskText });
-        $scope.taskText = "";
-    };
+        var _text = "Unknown";
+        if ($scope.taskText !== "") {
+            _text = $scope.taskText;
+        }
 
-    function resetTimer() {
-        clearInterval(timerInterval);
-        $scope.currentTime = $scope.selectedOption.value + ":" + "00";
-        timerDate.setMinutes($scope.selectedOption.value);
-        timerDate.setSeconds(0); // Switch
+        $scope.tasks.push({ start: startTime, stop: endTime, text: _text });
+        $scope.taskText = "";
     };
 
     function alertNotification() {
         //FlashTitle();
-        //if ($('#SoundSelection:checked').is(':checked')) {
-        //    PlaySound();
-        //}
+        if ($scope.sound) {
+            playSound();
+        }
         alertDesktopNotification();
     };
 
@@ -83,7 +111,7 @@
         if (window.webkitNotifications) {
             if (window.webkitNotifications.checkPermission() == 0) {
                 var notification_test = window.webkitNotifications.createNotification(
-                'Content/pom.png', 'Agile Task Complete', 'Time to take a break!');
+                'Content/logo.png', 'Agile Task Complete', 'Time to take a break!');
                 notification_test.ondisplay = function () { };
                 notification_test.onclose = function () { };
                 notification_test.onClick = function () { };
@@ -91,7 +119,12 @@
             }
         }
     };
-    
+
+    function playSound() {
+        snd1.play();
+        snd2.play();
+    }
+
     document.querySelector('#Startbtn').addEventListener('click', function () {
         if (window.webkitNotifications.checkPermission() === 0) { // 0 is PERMISSION_ALLOWED
             // function defined 
