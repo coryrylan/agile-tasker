13d9cf100bbbea33c8ce963e387611ea63280a72
Cory Bateman <splintercode.cb@gmail.com>
2014-10-12 10:38:34 -0500

    Rename

--- App/JavaScript/Config/bundle.js
+++ App/JavaScript/Config/bundle.js
@@ -766,293 +766,284 @@ this.current.$$route){var c={},f=this;e.forEach(Object.keys(a),function(b){f.cur
         Object.seal(win.notify);
     }
 }(window));
-"use strict";
+(function () {
+    'use strict';
 
-var app = angular.module('AgileTasker', [
-    'ngRoute',
-    'LocalForageModule',
-    'app.filters',
-    'app.services',
-    'app.directives',
-    'app.controllers'
-]);
+    var app = angular.module('AgileTasker', [
+        'ngRoute',
+        'LocalForageModule',
+        'app.filters',
+        'app.services',
+        'app.directives',
+        'app.controllers'
+    ]);
 
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: 'Partials/timer.html'
-    });
-    $routeProvider.when('/about', {
-        templateUrl: 'Partials/about.html'
-    });
-    $routeProvider.when('/history', {
-        templateUrl: 'Partials/history.html'
-    });
-    $routeProvider.when('/settings', {
-        templateUrl: 'Partials/settings.html'
-    });
-    $routeProvider.otherwise({ redirectTo: '/' });
-}]);
-'use strict';
-
-/* Controllers */
-var appControllers = angular.module('app.controllers', []);
-
-appControllers.controller('exampleCtrl', function () {
-    //..
-})
-
-appControllers.controller('TimerCtrl', ['$scope', '$localForage', 'userSettings', 'notification', function ($scope, $localForage, userSettings, notification) {
-    //#region Models
-    $scope.isPlaying = false;
-
-    $scope.settings = {
-        options: [{ value: 15, label: 15 }, { value: 20, label: 20 }, { value: 25, label: 25 }, { value: 30, label: 30 }],
-        currentTime: "",
-        selectedTime: {},
-        taskTextBox: ""
-    };
-
-    $scope.settings.selectedTime = $scope.settings.options[2];
-    $scope.settings.currentTime = $scope.settings.options[2].value + ":00";
-
-    // Bind userSettings service to local storage
-    $scope.userSettings = userSettings;
-    $localForage.bind($scope, {
-        key: 'userSettings',
-        defaultValue: userSettings,
-        storeName: 'StorageSettings'
-    });
-    //#endregion
-
-    //#region Globals
-    var startTime = new Date().toLocaleTimeString();
-    var endTime = new Date().toLocaleTimeString();
-    var timerInterval = 0;
-    var timerDate = new Date();
-    timerDate.setMinutes($scope.settings.selectedTime.value);
-    timerDate.setSeconds(0);
-    //#endregion
-
-    //#region Click Events
-    $scope.startTimer = startTimer;
-    $scope.stopTimer = stopTimer;
-    $scope.clearList = clearHistory;
-    $scope.toggleSound = toggleSound;
-    //#endregion
-
-    function startTimer() {
-        resetTimer();
-        timerInterval = setInterval(intervalTimer, 1000);
-        startTime = new Date().toLocaleTimeString();
-        $scope.isPlaying = true;
-
-        if (notify.permissionLevel() === notify.PERMISSION_DEFAULT) {
-            notify.requestPermission();
-        }
-    }
-
-    function stopTimer() {
-        resetTimer();
-        $scope.isPlaying = '';
-        document.title = 'Agile Tasker';
-    };
-
-    function clearHistory() {
-        $scope.userSettings.taskHistory = [];
-    };
-
-    function toggleSound() {
-        $scope.userSettings.sound.play = !$scope.userSettings.sound.play;
-        if ($scope.userSettings.sound.play === true) {
-            notification.playAudio();
-        }
-    };
-
-    //#region Timmer Helper Functons
-    function intervalTimer() {
-        if (timeIsUp()) {
-            $scope.stopTimer();
-            endTime = new Date().toLocaleTimeString();
-            console.log($scope.userSettings.sound.play);
-            notification.notify($scope.userSettings.sound.play);
-            saveTaskToHistory();
-        } else {
-            timerDate.setSeconds(timerDate.getSeconds() - 1);
-            $scope.settings.currentTime = getCurrentTime(timerDate);
-            document.title = $scope.settings.currentTime;
-        }
-        $scope.$apply();
-    }
-
-    function timeIsUp() {
-        return (timerDate.getMinutes() === 0 && timerDate.getSeconds() === 0);
-    }
-
-    function resetTimer() {
-        clearInterval(timerInterval);
-        $scope.settings.currentTime = $scope.settings.selectedTime.value + ":" + "00";
-        timerDate.setMinutes($scope.settings.selectedTime.value);  // $scope.settings.selectedTime.value
-        timerDate.setSeconds(0);                                   // Test Switch 
-    }
-
-    function getCurrentTime(currentTime) {
-        var minutes = currentTime.getMinutes();
-        var seconds = currentTime.getSeconds();
-
-        if (minutes < 10) {
-            minutes = '0' + minutes;
-        }
-
-        if (seconds < 10) {
-            seconds = '0' + seconds;
-        }
-
-        return minutes + ':' + seconds;
-    }
-
-    function saveTaskToHistory() {
-        var _text = "Unknown";
-        if ($scope.settings.taskTextBox !== "") {
-            _text = $scope.settings.taskTextBox;
-        }
-
-        $scope.userSettings.taskHistory.push({ start: startTime, stop: endTime, text: _text });
-        $scope.settings.taskTextBox = "";
-    }
-    //#endregion
-}])
-'use strict';
-
-/* Directives */
-var appDirectives = angular.module('app.directives', []);
-
-appDirectives.directive('example', function () {
-    //..
-})
-
-appDirectives.directive('history', function () {
-    return {
-        restrict: 'E',
-        replace: true,
-        transclude: false,
-        template: '<a href="">' +
-                    'Test' +
-                  '</a>',
-        link: function (scope, element, attrs) {
-            // DOM manipulation/events here!
-        }
-    };
-})
-
-appDirectives.directive('appVersion', ['version', function (version) {
-    return function (scope, elm, attrs) {
-        elm.text(version);
-    };
-}])
-
-appDirectives.directive('clock', ['$timeout', 'dateFilter', function ($timeout, dateFilter) { // http://jsdo.it/can.i.do.web/zHbM
-    return function (scope, element, attrs) {
-        var timeoutId; // timeoutId, so that we can cancel the time updates
-
-        // schedule update in one second
-        function updateLater() {
-            // save the timeoutId for canceling
-            timeoutId = $timeout(function () {
-                element.text(dateFilter(new Date(), 'shortTime'));
-                updateLater(); // schedule another update
-            }, 1000);
-        }
-
-        // listen on DOM destroy (removal) event, and cancel the next UI update
-        // to prevent updating time ofter the DOM element was removed.
-        element.bind('$destroy', function () {
-            $timeout.cancel(timeoutId);
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'Partials/timer.html'
         });
+        $routeProvider.when('/about', {
+            templateUrl: 'Partials/about.html'
+        });
+        $routeProvider.when('/history', {
+            templateUrl: 'Partials/history.html'
+        });
+        $routeProvider.when('/settings', {
+            templateUrl: 'Partials/settings.html'
+        });
+        $routeProvider.otherwise({ redirectTo: '/' });
+    }]);
+})();
+(function () {
+    'use strict';
 
-        updateLater(); // kick off the UI update process.
-    }
-}]);
-'use strict';
+    var appControllers = angular.module('app.controllers', []);
 
-/* Filters */
-var appFilters = angular.module('app.filters', []);
+    appControllers.controller('TimerCtrl', ['$scope', '$localForage', 'userSettings', 'notification', function ($scope, $localForage, userSettings, notification) {
+        //#region Models
+        $scope.isPlaying = false;
 
-appFilters.filter('example', function () {
-    //..
-})
+        $scope.settings = {
+            options: [{ value: 15, label: 15 }, { value: 20, label: 20 }, { value: 25, label: 25 }, { value: 30, label: 30 }],
+            currentTime: "",
+            selectedTime: {},
+            taskTextBox: ""
+        };
 
-appFilters.filter('interpolateVersion', ['version', function (version) {
-    return function (text) {
-        return String(text).replace(/\%VERSION\%/mg, version);
-    };
-}]);
-'use strict';
+        $scope.settings.selectedTime = $scope.settings.options[2];
+        $scope.settings.currentTime = $scope.settings.options[2].value + ":00";
 
-/* Services */
-var appServices = angular.module('app.services', []).value('version', '1.5.4');
+        // Bind userSettings service to local storage
+        $scope.userSettings = userSettings;
+        $localForage.bind($scope, {
+            key: 'userSettings',
+            defaultValue: userSettings,
+            storeName: 'StorageSettings'
+        });
+        //#endregion
 
-appServices.factory('example', function () {
-    //..
-})
+        //#region Globals
+        var startTime = new Date().toLocaleTimeString();
+        var endTime = new Date().toLocaleTimeString();
+        var timerInterval = 0;
+        var timerDate = new Date();
+        timerDate.setMinutes($scope.settings.selectedTime.value);
+        timerDate.setSeconds(0);
+        //#endregion
 
-appServices.factory('userSettings', function () {
-    _userSettings = {
-        sound: { play: true },
-        taskHistory: []
-    }
-    return _userSettings;
-})
+        //#region Click Events
+        $scope.startTimer = startTimer;
+        $scope.stopTimer = stopTimer;
+        $scope.clearList = clearHistory;
+        $scope.toggleSound = toggleSound;
+        //#endregion
 
-appServices.factory('notification', function () {
+        function startTimer() {
+            resetTimer();
+            timerInterval = setInterval(intervalTimer, 1000);
+            startTime = new Date().toLocaleTimeString();
+            $scope.isPlaying = true;
 
-    var audio = new Audio();
-    audio.src = Modernizr.audio.ogg ? 'Content/Audio/chime.ogg' :
-                Modernizr.audio.mp3 ? 'Content/Audio/chime.mp3' :
-                                      'Content/Audio/chime.m4a';
+            if (notify.permissionLevel() === notify.PERMISSION_DEFAULT) {
+                notify.requestPermission();
+            }
+        }
 
-    function nativeNotification() {
-        if (notify.permissionLevel() === notify.PERMISSION_DEFAULT) {
-            notify.requestPermission();
-        } else if (notify.permissionLevel() === notify.PERMISSION_GRANTED) {
-            notify.createNotification('Agile Task Complete', {
-                body: 'Time to take a break!',
-                icon: 'Content/Images/icon.png'
+        function stopTimer() {
+            resetTimer();
+            $scope.isPlaying = '';
+            document.title = 'Agile Tasker';
+        }
+
+        function clearHistory() {
+            $scope.userSettings.taskHistory = [];
+        }
+
+        function toggleSound() {
+            $scope.userSettings.sound.play = !$scope.userSettings.sound.play;
+            if ($scope.userSettings.sound.play === true) {
+                notification.playAudio();
+            }
+        }
+
+        //#region Timmer Helper Functons
+        function intervalTimer() {
+            if (timeIsUp()) {
+                $scope.stopTimer();
+                endTime = new Date().toLocaleTimeString();
+                console.log($scope.userSettings.sound.play);
+                notification.notify($scope.userSettings.sound.play);
+                saveTaskToHistory();
+            } else {
+                timerDate.setSeconds(timerDate.getSeconds() - 1);
+                $scope.settings.currentTime = getCurrentTime(timerDate);
+                document.title = $scope.settings.currentTime;
+            }
+            $scope.$apply();
+        }
+
+        function timeIsUp() {
+            return (timerDate.getMinutes() === 0 && timerDate.getSeconds() === 0);
+        }
+
+        function resetTimer() {
+            clearInterval(timerInterval);
+            $scope.settings.currentTime = $scope.settings.selectedTime.value + ":" + "00";
+            timerDate.setMinutes($scope.settings.selectedTime.value);  // $scope.settings.selectedTime.value
+            timerDate.setSeconds(0);                                   // Test Switch 
+        }
+
+        function getCurrentTime(currentTime) {
+            var minutes = currentTime.getMinutes();
+            var seconds = currentTime.getSeconds();
+
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+
+            if (seconds < 10) {
+                seconds = '0' + seconds;
+            }
+
+            return minutes + ':' + seconds;
+        }
+
+        function saveTaskToHistory() {
+            var _text = "Unknown";
+            if ($scope.settings.taskTextBox !== "") {
+                _text = $scope.settings.taskTextBox;
+            }
+
+            $scope.userSettings.taskHistory.push({ start: startTime, stop: endTime, text: _text });
+            $scope.settings.taskTextBox = "";
+        }
+        //#endregion
+    }]);
+})();
+
+(function () {
+    'use strict';
+
+    var appDirectives = angular.module('app.directives', []);
+
+    appDirectives.directive('history', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            transclude: false,
+            template: '<a href="">' +
+                        'Test' +
+                      '</a>',
+            link: function (scope, element, attrs) {
+
+            }
+        };
+    });
+
+    appDirectives.directive('appVersion', ['version', function (version) {
+        return function (scope, elm, attrs) {
+            elm.text(version);
+        };
+    }]);
+
+    appDirectives.directive('clock', ['$timeout', 'dateFilter', function ($timeout, dateFilter) { // http://jsdo.it/can.i.do.web/zHbM
+        return function (scope, element, attrs) {
+            var timeoutId; // timeoutId, so that we can cancel the time updates
+
+            // schedule update in one second
+            function updateLater() {
+                // save the timeoutId for canceling
+                timeoutId = $timeout(function () {
+                    element.text(dateFilter(new Date(), 'shortTime'));
+                    updateLater(); // schedule another update
+                }, 1000);
+            }
+
+            // listen on DOM destroy (removal) event, and cancel the next UI update
+            // to prevent updating time ofter the DOM element was removed.
+            element.bind('$destroy', function () {
+                $timeout.cancel(timeoutId);
             });
-        } else if (notify.permissionLevel() === notify.PERMISSION_DENIED) {
-            alert("Agile Task Complete");
-        } else {
-            alert("Agile Task Complete");
+
+            updateLater(); // kick off the UI update process.
+        };
+    }]);
+})();
+(function () {
+    'use strict';
+
+    var appFilters = angular.module('app.filters', []);
+
+    appFilters.filter('interpolateVersion', ['version', function (version) {
+        return function (text) {
+            return String(text).replace(/\%VERSION\%/mg, version);
+        };
+    }]);
+})();
+(function () {
+    'use strict';
+
+    var appServices = angular.module('app.services', []).value('version', '1.5.4');
+
+    appServices.factory('userSettings', function () {
+        var _userSettings = {
+            sound: { play: true },
+            taskHistory: []
+        };
+        return _userSettings;
+    });
+
+    appServices.factory('notification', function () {
+
+        var audio = new Audio();
+        audio.src = Modernizr.audio.ogg ? 'Content/Audio/chime.ogg' :
+                    Modernizr.audio.mp3 ? 'Content/Audio/chime.mp3' :
+                                          'Content/Audio/chime.m4a';
+
+        function nativeNotification() {
+            if (notify.permissionLevel() === notify.PERMISSION_DEFAULT) {
+                notify.requestPermission();
+            } else if (notify.permissionLevel() === notify.PERMISSION_GRANTED) {
+                notify.createNotification('Agile Task Complete', {
+                    body: 'Time to take a break!',
+                    icon: 'Content/Images/icon.png'
+                });
+            } else if (notify.permissionLevel() === notify.PERMISSION_DENIED) {
+                alert("Agile Task Complete");
+            } else {
+                alert("Agile Task Complete");
+            }
         }
-    }
 
-    function toggleAudio(playSound) {
-        if (playSound === true) {
-            audio.pause();
-            audio.currentTime = 0;
-            audio.play();
+        function toggleAudio(playSound) {
+            if (playSound === true) {
+                audio.pause();
+                audio.currentTime = 0;
+                audio.play();
+            }
         }
-    }
 
-    function vibrationNotification() {
-        // enable vibration support
-        navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
+        function vibrationNotification() {
+            // enable vibration support
+            navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
-        if (navigator.vibrate) {
-            navigator.vibrate([500, 200, 500]);
+            if (navigator.vibrate) {
+                navigator.vibrate([500, 200, 500]);
+            }
         }
-    }
 
-    return {
-        notify: function (playSound) {
-            vibrationNotification();
-            setTimeout(function () { nativeNotification(); toggleAudio(playSound); }, 1600);
-            // Set timeout because html5 Vibrate API does not take a callback ( Alert stops vibration ) :(
-        },
+        return {
+            notify: function (playSound) {
+                vibrationNotification();
+                setTimeout(function () { nativeNotification(); toggleAudio(playSound); }, 1600);
+                // Set timeout because html5 Vibrate API does not take a callback ( Alert stops vibration ) :(
+            },
 
-        playAudio: function () {
-            audio.pause();
-            audio.currentTime = 0;
-            audio.play();
-        }
-    }
-});
+            playAudio: function () {
+                audio.pause();
+                audio.currentTime = 0;
+                audio.play();
+            }
+        };
+    });
+})();
--- Specs/SpecRunners/plato-reporter.bat
+++ Specs/SpecRunners/plato-reporter.bat
@@ -1,1 +1,1 @@
-plato -r -d Reports -t "Agile Tasker" ../../App/Js/Src/
+plato -r -d Reports -t "Agile Tasker" ../../App/JavaScript/Src/
